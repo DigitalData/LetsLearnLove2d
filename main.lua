1987b5466cc6ff149c078e4de0d5d424219ea8fd
DigitalData <legoxavierlocketravers@gmail.com>
2017-02-23 22:35:45 +0700

    25 February, 2017 - The Button Hover Update
    + Added Hover effects for menu buttons
    + Added ability to quit
    + Added ability to open options (WIP Menu)
    + Improved Button Placement on Screen

--- main.lua
+++ main.lua
@@ -4,6 +4,7 @@ function love.load()
 onMenu = true;
 menu = require("menu");
 menu.loadMenu();
+willQuit = false;
 end
 
 function love.update(dt)
@@ -24,4 +25,21 @@ function love.focus(focus)
   if not focus then
     onMenu = true;
   end
+end
+
+function love.mousepressed(x, y, button, isTouch)
+  if button == 1 then
+    willQuit = menu.mousepressed(x,y);
+    if(willQuit)then
+      love.event.quit(exitstatus)
+    end
+  end
+end
+
+function love.keypressed(key, scancode, isrepeat)
+
+end
+
+function love.quit()
+  -- body...
 end
--- menu.lua
+++ menu.lua
@@ -4,35 +4,57 @@ function loadMenu()
   title = love.graphics.newFont("assets/fonts/titlefont.ttf",150);
   namea = 255;
   nameadown = true;
+  mainMenu = true;
+  options = false;
+  start = false;
+  mouseX = love.mouse.getX();
+  mouseY = love.mouse.getY();
+  screenX = 800;
+  screenY = 600;
+  fScreen = false;
+  gameV = 100;
+  musicV = 100;
+  buttonX = (love.graphics.getWidth() / 2) - 110;
+  mainButtonY = {};
 end
 
 function drawMenu()
   love.graphics.setFont(title);
-local shakex = math.random(0, 5);
-local shakey = math.random(0, 5);
+  local shakex = math.random(0, 5);
+  local shakey = math.random(0, 5);
 
-love.graphics.setColor(255, 255, 255, 255);
-love.graphics.print("This Little Game", (winW / 2 + shakex) - 200, 100 + shakey, 0,1,1);
+  love.graphics.setColor(255, 255, 255, 255);
+  love.graphics.print("This Little Game", (winW / 2 + shakex) - 200, 100 + shakey, 0,1,1);
 
-love.graphics.setColor(255, 255, 255, namea);
-love.graphics.print(" - Xavier Travers", (winW / 2 - 200), 210, 0, 0.5,0.5);
+  love.graphics.setColor(255, 255, 255, namea);
+  love.graphics.print(" - Xavier Travers", (winW / 2 - 200), 210, 0, 0.5,0.5);
 
-love.graphics.setColor(255, 255, 255, 255);
-love.graphics.rectangle("line", (love.graphics.getWidth() / 2) -110, 310, 200, 60);
-love.graphics.print("Start", (love.graphics.getWidth() / 2) -40, 310, 0, 0.4, 0.4);
-love.graphics.rectangle("line", (love.graphics.getWidth() / 2) -110, 380 + adh, 200, 60);
-love.graphics.print("Options", (love.graphics.getWidth() / 2) -50, 380 + adh, 0, 0.4, 0.4);
-love.graphics.rectangle("line", (love.graphics.getWidth() / 2) -110, 310 + (2*70) + 2*adh, 200, 60);
-love.graphics.print("Quit", (love.graphics.getWidth() / 2) -40, 310 + (2*70) + 2*adh, 0, 0.4, 0.4);
+  if(mainMenu)then
+    drawMainMenu();
+  elseif(options) then
+    drawOptions();
+  elseif(start)then
+  elseif(quit) then
+  end
 
 end
 
 function updateMenu(dt)
 
-winW = love.graphics.getWidth();
-winH = love.graphics.getHeight();
+  winW = love.graphics.getWidth();
+  winH = love.graphics.getHeight();
+  adh = love.graphics.getHeight() / 45;
 
-adh = love.graphics.getHeight() / 45;
+  mouseX = love.mouse.getX();
+  mouseY = love.mouse.getY();
+
+  if(mainMenu)then
+    updateMainMenu();
+  elseif(options) then
+    updateOptions();
+  elseif(start)then
+  elseif(quit) then
+  end
 
   if(nameadown)then
     if(namea < 50) then
@@ -49,8 +71,116 @@ adh = love.graphics.getHeight() / 45;
   end
 end
 
+function drawMainMenu()
+  local hoverNum = -1;
+  if (mouseX > buttonX) and (mouseX < buttonX+200) then
+    for i=0,2 do
+      if(mouseY > mainButtonY[i]) and (mouseY < mainButtonY[i] + 60)then
+        if i==0 then
+          hoverOver = true;
+          hoverNum = i;
+        elseif i==1 then
+          hoverOver = true;
+          hoverNum = i;
+        elseif i==2 then
+          hoverOver = true;
+          hoverNum = i;
+        end
+      end
+    end
+  end
+
+  love.graphics.setColor(255, 255, 255, 255);
+  for i=0,2 do
+    if( i == hoverNum )then
+      love.graphics.rectangle("fill", buttonX,mainButtonY[i] ,200 , 60);
+    else
+      love.graphics.rectangle("line", buttonX,mainButtonY[i] ,200 , 60);
+    end
+  end
+
+  love.graphics.setColor(255, 255, 255, 255);
+  if(hoverNum == 0) then
+    love.graphics.setColor(0,0,0,255);
+    love.graphics.print("Start", (love.graphics.getWidth() / 2) -40, mainButtonY[0], 0, 0.4, 0.4);
+    love.graphics.setColor(255, 255, 255, 255);
+    love.graphics.print("Options", (love.graphics.getWidth() / 2) -50, mainButtonY[1], 0, 0.4, 0.4);
+    love.graphics.print("Quit", (love.graphics.getWidth() / 2) -40, mainButtonY[2], 0, 0.4, 0.4);
+  elseif(hoverNum == 1) then
+    love.graphics.print("Start", (love.graphics.getWidth() / 2) -40, mainButtonY[0], 0, 0.4, 0.4);
+    love.graphics.setColor(0,0,0,255);
+    love.graphics.print("Options", (love.graphics.getWidth() / 2) -50, mainButtonY[1], 0, 0.4, 0.4);
+    love.graphics.setColor(255, 255, 255, 255);
+    love.graphics.print("Quit", (love.graphics.getWidth() / 2) -40, mainButtonY[2], 0, 0.4, 0.4);
+  elseif(hoverNum == 2) then
+    love.graphics.print("Start", (love.graphics.getWidth() / 2) -40, mainButtonY[0], 0, 0.4, 0.4);
+    love.graphics.print("Options", (love.graphics.getWidth() / 2) -50, mainButtonY[1], 0, 0.4, 0.4);
+    love.graphics.setColor(0,0,0,255);
+    love.graphics.print("Quit", (love.graphics.getWidth() / 2) -40, mainButtonY[2], 0, 0.4, 0.4);
+    love.graphics.setColor(255, 255, 255, 255);
+  else
+    love.graphics.print("Start", (love.graphics.getWidth() / 2) -40, mainButtonY[0], 0, 0.4, 0.4);
+    love.graphics.print("Options", (love.graphics.getWidth() / 2) -50, mainButtonY[1], 0, 0.4, 0.4);
+    love.graphics.print("Quit", (love.graphics.getWidth() / 2) -40, mainButtonY[2], 0, 0.4, 0.4);
+  end
+
+end
+
+function updateMainMenu()
+  for i=0,2 do
+    mainButtonY[i] = 310 + (i * 70) + adh;
+  end
+end
+
+function optionSwitch()
+
+  options = true;
+
+end
+
+function drawOptions()
+
+  love.graphics.print("Options WIP", 210, 370, 0, 0.8, 0.8);
+
+end
+
+function updateOptions()
+
+
+
+end
+
+function mousepressed(x, y)
+  if(mainMenu) then
+    if (x > buttonX) and (x < buttonX+200) then
+      for i=0,2 do
+        if(y > mainButtonY[i]) and (y < mainButtonY[i] + 60)then
+          if i==0 then
+            mainMenu = false;
+            start = true;
+
+          elseif i==1 then
+
+            mainMenu = false;
+            options = true;
+
+          elseif i==2 then
+
+            mainMenu = false;
+            quit = true;
+
+            return true;
+
+          end
+        end
+      end
+    end
+  end
+end
+
 m.loadMenu = loadMenu;
 m.updateMenu = updateMenu;
 m.drawMenu = drawMenu;
+m.mousepressed = mousepressed;
 
 return m;
